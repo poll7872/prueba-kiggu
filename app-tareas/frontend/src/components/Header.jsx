3e409ab5f433b98513b283318d1275ce3ab6992c
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-07 12:20:13 -0500

    Styles: component Header

--- app-tareas/frontend/src/components/Header.jsx
+++ app-tareas/frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
-    <header>
-      <h1>App Mis Tareas</h1>
+    <header className="w-full text-center my-8 mx-auto">
+      <h1 className="text-2xl text-white font-bold">App Mis Tareas</h1>
       <button type="button">Crear Tarea</button>
     </header>
   );
