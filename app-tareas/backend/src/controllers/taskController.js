734475002b65e1f61c9d09553d8b86afcda4499e
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-06 21:14:08 -0500

    Create: methods createTasks and getAll

--- app-tareas/backend/src/controllers/taskController.js
+++ app-tareas/backend/src/controllers/taskController.js
@@ -6,16 +6,19 @@ let nextTaskId = 1;
 
 export class TaskController {
   //Método para crear una nueva tarea
-  createTask(title, description, categories) {
-    const id = nextTaskId++;
-    const task = new Task(
-      id,
-      title,
-      description,
-      (status = "To Do"),
-      categories,
-    );
-    tasks[id] = task;
-    return task;
+  createTask(title, description, state = "To Do", categories) {
+    try {
+      const id = nextTaskId++;
+      const task = new Task(id, title, description, state, categories);
+      tasks[id] = task;
+      return task;
+    } catch (error) {
+      throw new Error(`Error al crear la tarea: ${error.message}`);
+    }
+  }
+
+  //Método para obtener las notas
+  getAllTasks() {
+    return Object.values(tasks);
   }
 }
