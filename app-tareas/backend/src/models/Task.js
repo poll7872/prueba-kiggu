9f974866aa65669835926f71ac34294d89dc0a73
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-06 20:43:28 -0500

    Config: folders

---
+++ app-tareas/backend/src/models/Task.js
@@ -0,0 +1,8 @@
+export class Task {
+  constructor(id, title, description, categories) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.categories = categories; //Array con strings de categories
+  }
+}
