e7fff3b7c406164cd3b8c15cf02686fb411c78cf
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-06 21:02:09 -0500

    Validations: model/task

--- app-tareas/backend/src/models/Task.js
+++ app-tareas/backend/src/models/Task.js
@@ -1,8 +1,20 @@
 export class Task {
-  constructor(id, title, description, categories) {
+  constructor(id, title, description, categories, status = "To Do") {
+    //Validar que el titulo no este vacio
+    if (!title || title.trim === "") {
+      throw new Error("El titulo no puede estar vacio");
+    }
+
+    //Solo se permite tres estados "To Do", "In Progress", "Done"
+    const validStatus = ["To Do", "In Progress", "Done"];
+    if (!validStatus.includes(status)) {
+      throw new Error("El estado debe de ser: To Do, In Progress o Done");
+    }
+
     this.id = id;
     this.title = title;
     this.description = description;
+    this.status = status;
     this.categories = categories; //Array con strings de categories
   }
 }
