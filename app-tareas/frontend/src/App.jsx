bf2d5807d659f1f957c8432ba27e36de2f791e5e
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-07 12:09:31 -0500

    Component: Estructure Header

--- app-tareas/frontend/src/App.jsx
+++ app-tareas/frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "../src/styles/global.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-2xl">Hola Mundo</h1>
+      <Header />
     </>
   );
 }
---
+++ app-tareas/frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header>
+      <h1>App Mis Tareas</h1>
+      <button type="button">Crear Tarea</button>
+    </header>
+  );
+}
