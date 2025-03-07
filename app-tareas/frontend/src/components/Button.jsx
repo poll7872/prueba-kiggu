cabd51e09951d20a54621b15f7c6c5d76a7d6eb2
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-07 12:28:44 -0500

    Styles: defined button

---
+++ app-tareas/frontend/src/components/Button.jsx
@@ -0,0 +1,13 @@
+export function Button({
+  type = "button", //Por defecto será button
+  color = "primary",
+  size = "medium",
+  onClick,
+  children,
+  icon,
+  iconPosition = "left",
+}) {
+  //Estilos base dle button
+  const baseStyles =
+    "font-semibold rounded-lg transition-all duration-200 focus:outline-none flex items-center justify-center";
+}
--- app-tareas/frontend/src/components/Header.jsx
+++ app-tareas/frontend/src/components/Header.jsx
@@ -2,7 +2,12 @@ export function Header() {
   return (
     <header className="w-full text-center my-8 mx-auto">
       <h1 className="text-2xl text-white font-bold">App Mis Tareas</h1>
-      <button type="button">Crear Tarea</button>
+      <button
+        className="p-2 font-bold bg-purple-900 text-white rounded-lg"
+        type="button"
+      >
+        Crear Tarea +
+      </button>
     </header>
   );
 }
