2a34bb5941204189228ad01513b05a07ec00826e
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-07 12:53:57 -0500

    Styles: modified component Header

--- app-tareas/frontend/src/components/Header.jsx
+++ app-tareas/frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 
 export function Header() {
   return (
-    <header className="w-full text-center my-8 mx-auto">
+    <header className="grid justify-items-center my-6 gap-3">
       <h1 className="text-2xl text-white font-bold">App Mis Tareas</h1>
       <Button icon={<AddCircleFilled className="text-xl" />}>
         Crear Tarea
